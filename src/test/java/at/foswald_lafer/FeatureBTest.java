47b7b078f14a8071fcfda9c031ba2cc36decfb9e
christoph <christoph.lafer@htl-klu.at>
2024-08-21 13:46:55 +0200

    Reset Annotations

--- src/test/java/at/foswald_lafer/FeatureBTest.java
+++ src/test/java/at/foswald_lafer/FeatureBTest.java
@@ -1,8 +1,9 @@
 package at.foswald_lafer;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FeatureBTest {
 
